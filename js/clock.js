48d5539c56bb81d370a121315fd398671f9d6a93
Yulekk13 <102365739+Yulekk13@users.noreply.github.com>
2023-11-15 12:01:56 +0100

    Small changes in visualisation

--- js/clock.js
+++ js/clock.js
@@ -3,6 +3,17 @@ function displayTime() {
   var hrs = dataTime.getHours();
   var mins = dataTime.getMinutes();
   var secs = dataTime.getSeconds();
+
+  if (hrs < 10) {
+    hrs = "0" + hrs;
+  }
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
+  if (secs < 10) {
+    secs = "0" + secs;
+  }
+
   var session = document.getElementById("session");
 
   document.getElementById("hours").innerHTML = hrs;
