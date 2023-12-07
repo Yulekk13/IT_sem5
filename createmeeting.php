d3a616c06044000bb37a272f64b5475b7a678091
Szymon <szymi17s@interia.pl>
2023-12-07 10:55:45 +0100

    Solved issues with data and time

--- createmeeting.php
+++ createmeeting.php
@@ -1,56 +1,58 @@
 <?php
 
-// Assuming your form uses POST method
-$Name = isset($_POST['Name']) ? $_POST['Name'] : '';
-$Subject = isset($_POST['Subject']) ? $_POST['Subject'] : '';
-$Date = isset($_POST['Date']) ? $_POST['Date'] : '';
-$Time = isset($_POST['Time']) ? $_POST['Time'] : '';
-$Description = isset($_POST['Description']) ? $_POST['Description'] : '';
+// // Assuming your form uses POST method
+// $Name = isset($_POST['Name']) ? $_POST['Name'] : '';
+// $Subject = isset($_POST['Subject']) ? $_POST['Subject'] : '';
+// $Date = isset($_POST['Date']) ? $_POST['Date'] : '';
+// $Time = isset($_POST['Time']) ? $_POST['Time'] : '';
+// $Description = isset($_POST['Description']) ? $_POST['Description'] : '';
 
-// Validate input if needed
-// Example: You can use regex, filter_var, or other validation methods here
+// // Validate input if needed
+// // Example: You can use regex, filter_var, or other validation methods here
 
-// Database connection
-$conn = new mysqli('localhost', 'root', '', 'users');
+// // Database connection
+// $conn = new mysqli('localhost', 'root', '', 'users');
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
-// Prepare and execute the query
-$stmt = $conn->prepare("INSERT INTO meetings (Name, Subject, Date, Time, Description) VALUES (?, ?, ?, ?, ?)");
+// // Prepare and execute the query
+// $stmt = $conn->prepare("INSERT INTO meetings (Name, Subject, Date, Time, Description) VALUES (?, ?, ?, ?, ?)");
 
-if (!$stmt) {
-    die("Error in preparing statement: " . $conn->error);
-}
-
-$stmt->bind_param("sssss", $Name, $Subject, $Date, $Time, $Description);
-
-if (!$stmt->execute()) {
-    die("Error in executing statement: " . $stmt->error);
-}
-
-$stmt->close();
-$conn->close();
-
-header("Location: meetings.php");
-
-// $Name = $_POST['Name'];
-// $Subject = $_POST['Subject'];
-// $Date = $_POST['Date'];
-// $Time = $_POST['Time'];
-// $Description = $_POST['Description'];
-
-
-// //Database connection
-// $conn = new mysqli('localhost', 'root','','users');
-
-// $stmt = $conn->prepare("insert into meetings(Name, Subject, Date, Time, Description)
-// values (?,?,?,?,?)");
+// if (!$stmt) {
+//     die("Error in preparing statement: " . $conn->error);
+// }
 
 // $stmt->bind_param("sssss", $Name, $Subject, $Date, $Time, $Description);
-// $stmt->execute();
+
+// if (!$stmt->execute()) {
+//     die("Error in executing statement: " . $stmt->error);
+// }
+
 // $stmt->close();
+// $conn->close();
+
+// header("Location: meetings.php");
+
+$Name = $_POST['Name'];
+$Subject = $_POST['Subject'];
+$Date = $_POST['date'];
+$Time = $_POST['time'];
+$Description = $_POST['Description'];
+
+
+//Database connection
+$conn = new mysqli('localhost', 'root','','users');
+
+$stmt = $conn->prepare("insert into meetings(Name, Subject, Date, Time, Description)
+values (?,?,?,?,?)");
+
+$stmt->bind_param("sssss", $Name, $Subject, $Date, $Time, $Description);
+$stmt->execute();
+$stmt->close();
+
+header("Location: meetings.php");
 
 ?>
