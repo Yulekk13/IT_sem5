00af2f618f8174515afd4fb27c452ce7c3b7c852
Yulekk13 <pm302704@student.polsl.pl>
2023-12-11 17:09:16 +0100

    Delating old meetings working

--- config/delate.php
+++ config/delate.php
@@ -3,17 +3,17 @@
     function delate_old() {
         global $con;
         //$query = "select id, time, date form meetings";
+        //echo date("G h:i:s", strtotime('-1 hour'))
         
-        $ndate = date("y-m-d")
-        $ntime = time("h-m-s", strtotime('-1 hour')); 
-        mysqli_query($con, "DELETE FROM meetings WHERE date == $ndate AND time < '$ntime' ");
+        $ndate = date("y-m-d");
+        $ntime = date("G h:i:s"); 
+        mysqli_query($con, "DELETE FROM meetings WHERE (Date < '$ndate') AND (Time < '$ntime');");
     }
 
 ?>
 
-if date == adate
+<!-- if date == adate
     if time == atime - 1h
         delate
 if date < adate
-    delate
-
+    delate -->
